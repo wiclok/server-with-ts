71576b27611e82adef47e3d88d8a22a2b282803a
Cespedes Brian <cespedes.aguilera.brian@gmail.com>
2024-08-29 16:05:14 -0300

    Environment variables for database connection have been added

--- src/config/environments.ts
+++ src/config/environments.ts
@@ -2,3 +2,8 @@ import 'dotenv/config'
 
 export const PORT: string = process.env.PORT!
 export const HOST: string = process.env.HOST!
+export const DB_NAME: string = process.env.DB_NAME!
+export const DB_USER: string = process.env.DB_USER!
+export const DB_PASS: string = process.env.DB_PASS!
+export const DB_HOST: string = process.env.DB_HOST!
+export const DB_DIALECT: string = process.env.DB_DIALECT!
